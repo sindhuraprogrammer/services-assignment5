a213bacbe7a0b53dae3c58a730b38fcadf9bf737
sindhuraprogrammer <sindhura.programmer@gmail.com>
2019-11-20 21:42:54 +1300

    copied starter file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ActiveUsersComponent, InactiveUsersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
