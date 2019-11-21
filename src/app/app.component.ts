13630d6aba215aa7d3f1dd5e7cf97329851b7d24
sindhuraprogrammer <sindhura.programmer@gmail.com>
2019-11-21 18:46:04 +1300

    implemented counterservice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import {CounterService} from './counter.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[CounterService]
 })
 export class AppComponent {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
-
+constructor(private counterService : CounterService){}
   onSetToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.countActions();
   }
 
   onSetToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+     this.counterService.countActions();
   }
 }
