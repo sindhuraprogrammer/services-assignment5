ef003b1e37b385dafa01cd2a06008716ea48d4ab
sindhuraprogrammer <sindhura.programmer@gmail.com>
2019-11-22 12:14:16 +1300

    removed hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
 import { UsersServiceService } from './users-service.service';
@@ -11,7 +11,7 @@ import {CounterService} from './counter.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ActiveUsersComponent, InactiveUsersComponent ],
+  declarations: [ AppComponent,  ActiveUsersComponent, InactiveUsersComponent ],
   bootstrap:    [ AppComponent ],
   providers: [UsersServiceService,CounterService]
 })
